9a1fd6a9a4573922955c50b0672782e322a14872
inxx <noobish.in@gmail.com>
2020-11-27 16:52:30 +0900

    시작시작 ∑d(°∀°d)

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 8080;
+
+// router
+const indexRouter = require('./routes/index');
+const adminRouter = require('./routes/admin');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', indexRouter);
+app.get('/admin', adminRouter);
+
+app.listen(port, function(err) {
+  console.log('Connected port' + port);
+  if (err) {
+    return console.log('Found error', err);
+  }
+})
